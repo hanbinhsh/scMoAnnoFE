6ae6d1680568596af311b3ac8e47ada7097e05a0
aweiawei666 <2115409491@qq.com>
2024-09-08 13:29:17 +0800

    1

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -176,6 +176,7 @@ export default {
                   }, 2000);
                 } else {
                   console.error("Registration failed:", response.data.msg);
+                  alert(response.data.msg);
                 }
               })
               .catch(error => {
